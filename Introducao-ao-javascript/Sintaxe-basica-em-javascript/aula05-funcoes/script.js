7e1a29dd36c76df40c4c68c35c66e5a4edb05278
Daidgydfreitas <dennergti@gmail.com>
2022-01-07 18:59:33 -0300

    Estrutura de funções

---
+++ Introducao-ao-javascript/Sintaxe-basica-em-javascript/aula05-funcoes/script.js
@@ -0,0 +1,9 @@
+/* function funcao(){
+    console.log('Belaza galera!')
+}
+funcao(); */
+
+function mensagem (primeiro, segundo){
+    console.log(primeiro, segundo)
+}
+mensagem('tudo certo', 'amigo!');
