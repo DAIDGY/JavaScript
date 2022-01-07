07421c5b671760d8bfa7c6b5c94d454bf141a81e
Daidgydfreitas <dennergti@gmail.com>
2022-01-07 16:45:19 -0300

    estrutura de condicionais

--- Introducao-ao-javascript/Sintaxe-basica-em-javascript/aula04-estrutura condicionais/script.js
+++ Introducao-ao-javascript/Sintaxe-basica-em-javascript/aula04-estrutura condicionais/script.js
@@ -1,17 +1,19 @@
-var jogador1 = -1;
+/* var jogador1 = 1;
 var jogador2 = 0;
 var placar;
 
-// if ternário
+// if ternário de uma linha só
 jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são válidos'): console.log('Jogadores Inválidos');
 
 //usando if
-if(jogador1 > 0){
-    console.log('Jogador 1 marcou ponto');    
+if(jogador1 > 0 && jogador2 == 0){
+    console.log('Jogador 1 marcou ponto');
+    placar = jogador1 > jogador2;    
 }
 //usando else if
-else if (jogador2 > 0){
+else if (jogador2 > 0 && jogador1 == 0){
     console.log('Jogador 2 marcou ponto');
+    placar = jogador2 > jogador1;
 }
 //usando else
 else{
@@ -19,18 +21,18 @@ else{
 }
 
 // usando o switch case
-/* switch (placar){
+switch (placar){
     case placar = jogador1 > jogador2:
         console.log('Jogador 1 ganhou');
         break;
-    case placar = jogador2 > jogadro1:
+    case placar = jogador2 > jogador1:
         console.log('Jogador 2 ganhou');
         break;
     default:
-        console.log('Ninguém ganhou')
+        console.log('Ninguém ganhou');
 }
  */
-
+ 
 let array = ['valor1', 'valor2','valor3', 'valor4', 'valor5'];
 
 let object = {
@@ -42,5 +44,36 @@ let object = {
     console.log(indice);
 } */
 
-//for/in executa  a repetição a partir de uma propriedade
+//for/in executa  a repetição a partir de uma propriedade.
 //com o array
+/* for (let i in array){
+    console.log(i)
+} */
+
+//com object
+/* for(i in object){
+    console.log(i);
+} */
+
+// for/Of - executa repetição apartir de um valor
+/* for (i of array){
+    console.log(i)
+} */
+
+//com object
+/* for (i of object.propriedade1){
+    console.log(i)
+} */
+
+// usando a condição while executa uma funação até que ela seja verdadeira
+var a = 0;
+
+while (a < 20){
+    a++;
+    console.log(a);
+}
+
+do{
+    a++;
+    console.log(a);
+}while(a < 20)
