0c99e4bea8b9ac2a3907fcb6d687ff5af287c7cb
Daidgydfreitas <dennergti@gmail.com>
2022-01-07 23:12:23 -0300

    Criando uma calculadora com javascript

---
+++ Introducao-ao-javascript/Sintaxe-basica-em-javascript/aula06-aprofundando-em-funcao/script.js
@@ -0,0 +1,20 @@
+//tipos de função
+
+//declarativas
+/* function funcao(){
+    console.log('Esta é uma mensagem de uma função declarativa');
+}
+funcao(); */
+
+//expressão de função
+//com nomeação
+/* var funcao = function funcao(){
+    console.log('Essa é uma mensagem de função de expressão');
+}
+funcao(); */
+
+// arrow function
+/* var funcao = () => {
+    console.log('Sou uma arrow function')
+}
+funcao(); */
