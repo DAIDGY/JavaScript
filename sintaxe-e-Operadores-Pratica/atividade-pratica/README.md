17e36aad28d68d8165ab00e3e61c2f5e00ed062c
Daidgydfreitas <dennergti@gmail.com>
2022-01-26 10:21:09 -0300

    Atividade de funções

--- sintaxe-e-Operadores-Pratica/atividade-pratica/README.md
+++ sintaxe-e-Operadores-Pratica/atividade-pratica/README.md
@@ -5,4 +5,6 @@
 *   Crie uma função que recebe dois numeros como parâmetros
 *   Confira se os numeros são iguais.
 *   Confira se a soma dos números é maior que 10 ou menor que 20.
-*   Retornw uma string dizendo "Os numeros (num1) e (num2) não/são iguais. Sua soma é (soma), que é maior/  menor que 10 e maior /menor que 10. 
+*   Retornw uma string dizendo "Os numeros (num1) e (num2) não/são iguais. Sua soma é (soma), que é maior/  menor que 10 e maior /menor que 20. 
+
+*   Fazendo o tratamento de erros.
--- sintaxe-e-Operadores-Pratica/atividade-pratica/atividade-function.js
+++ sintaxe-e-Operadores-Pratica/atividade-pratica/atividade-function.js
@@ -1,13 +1,69 @@
-function comparaNumero(num1, num2) {
-    const saoIguais = num1 === num2;
+//escrevendo meu código com boas práticas de forma enxuta.
+
+function comparaNumeros(num1, num2){
+    if (!num1 || !num2) return 'Defina dois números!';
+    const primeiraFrase = criaPrimeiraFrase (num1, num2);
+    const segundaFrase = criaSegundaFrase(num1, num2);
+}
+function criaPrimeiraFrase(num1, num2){
+    let saoIguais = '';
+
+    //Nesse caso a condição será o "não" são iguais.
+    if(num1 !== num2){
+        saoIguais = 'não';
+    }
+
+    return 'Os numeros ${num1} e ${num2} ${saoIguais} são iguais.';
+}
+
+function criaSegundaFrase(num1,num2){
     const soma = num1 + num2;
 
-    if (saoIguais){
+    let resultado10 = 'menor';
+    let resultado20 = 'menor';
+
+    const compara10 = soma > 10;
+    const compara20 = soma > 20;
+
+    if (compara10){
+        resultado10 = 'maior';
+    }
+    if (compara20){
+        resultado20 = 'maior';
+    }
+
+    return 'Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.';
+}
+
+console.log(comparaNumeros(10, 10));
+
+
+
+
+
+//Criando uma função que recebe dois parametros(num1, num2)//
+
+/* function comparaNumero(num1, num2) {
+
+    /**
+     * Confira se os numeros são iguas( nesse 
+     * caso foi utilisado a variável (const *"saoIguais que recebe os dois parametros *usando o sinal (===) que verifica a *igualdade e o tipo de dados ")
+     */
+    //const saoIguais = num1 === num2;
+    /* Confira se a soam dos numeros é maior que 10 ou menor que 20. No parametro abaixo a soma receberá a soma dos dois valores  
+    const soma = num1 + num2;
+
+    /*Aqui está uma condição se um for igual apresenta uma respost, se não outra.*/
+    //if (saoIguais){
+    /*
         return "São iguais";
+    
+    return "Não são iguais"; */
+    
+    //if ternario
+       /* return saoIguais ? "São Iguais" : "não são iguais";
     }
     return "Não são iguais"
 }
-console.log(comparaNumero(1, 2))
-
- /* if ternário*/
-    /* return saoIguais ? "São Iguais" : "não são iguais";*/
+console.log(comparaNumero())
+*/
